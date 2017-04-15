ba38a1d8f21e8f4f6477f55c9c1414a015bc433f
AJava <479741918@qq.com>
2017-04-15 11:22:09 +0800

    Add files via upload

---
+++ Zifuchuan.java
@@ -0,0 +1,18 @@
+package net;
+
+public class Zifuchuan {
+
+	public static void main(String[] args) {
+		String a =new String ("qwe");
+		String b =new String ("asd");
+		String c =a+
+				" "+
+				b+
+				" "+
+				b+
+				" "+
+				a;
+		System.out.print(c);
+	}
+
+}
