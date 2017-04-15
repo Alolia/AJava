4747631d5696fb274e8bc96386c63b4d5947acce
AJava <479741918@qq.com>
2017-04-15 14:33:03 +0800

    Add files via upload

---
+++ StringTest.java
@@ -0,0 +1,12 @@
+package net;
+
+public class StringTest {
+
+	public static void main(String[] args) {
+		String a = "hahahaoisdfndhaosdhguuaspvhabpuauf";
+		int size = a.lastIndexOf("");
+		System.out.println(size);
+		System.out.println(a.length());
+	}
+
+}
--- Zifuchuan.java
+++ Zifuchuan.java
@@ -3,8 +3,8 @@ package net;
 public class Zifuchuan {
 
 	public static void main(String[] args) {
-		String a =new String ("qwe");
-		String b =new String ("asd");
+		String a =new String ("q w e");
+		String b =new String ("a s d");
 		String c =a+
 				" "+
 				b+
